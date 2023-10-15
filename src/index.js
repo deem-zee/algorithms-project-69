4c64dc862c16183f992e530037dfcd40491b51be
deem-zee <58269447+deem-zee@users.noreply.github.com>
2023-10-15 12:37:06 +0300

    linter added
    linter air-bnb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 function search(docs, keyWord) {
-    const filtereArr = docs.filter(doc =>{ 
-        let regex = new RegExp('\\b' + keyWord + '\\b');
-        return doc.text.match(regex);
-    });
-    return filtereArr.map(doc => String(doc.id));
+  const filtereArr = docs.filter((doc) => {
+    const regex = new RegExp(`\\b${keyWord}\\b`);
+    return doc.text.match(regex);
+  });
+  return filtereArr.map((doc) => String(doc.id));
 }
 
 const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
@@ -12,4 +12,4 @@ const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const docs = [doc1, doc2, doc3];
 
 const result = search(docs, 'shoot');
-console.log(result)
+// console.log(result);
